76951f1e12752a21c80a174ecf11d63652553139
Ruben <167398241+rumacon@users.noreply.github.com>
2024-10-15 02:21:06 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excusa").innerHTML = excusas();
+  document.querySelector("#boton").addEventListener("click", () => {
+    document.querySelector("#excusa").innerHTML = excusas();
+  });
 };
+
+function excusas() {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let sujeto = Math.floor(Math.random() * who.length);
+  let verbo = Math.floor(Math.random() * action.length);
+  let que = Math.floor(Math.random() * what.length);
+  let cuando = Math.floor(Math.random() * when.length);
+
+  return (
+    who[sujeto] + " " + action[verbo] + " " + what[que] + " " + when[cuando]
+  );
+}
